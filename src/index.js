08fee9f276fdeeed8fad4d860b92a70d9458c1d4
Brijen Shah <135441289+brijenshahAPIs@users.noreply.github.com>
2023-06-03 10:42:00 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./routes/userRoutes"); 
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/", (req, res) =>{
+    res.send("Notes API From CheezyCode");
+});
+
+const PORT = 5100;
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.sti80hs.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port no. " + PORT);
+    });
+})
+.catch((error)=>{
+    console.log(error);
+})
